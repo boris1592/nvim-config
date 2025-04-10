749fab9536124eb6698b2cc292ae5ca63264f7eb
boris1592 <buzin.boria@gmail.com>
2025-04-11 01:35:20 +0300

    feat: svelte syntax

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'lua', 'vim', 'vimdoc', 'go', 'javascript', 'typescript', 'nix', 'typst', 'sql' },
+            ensure_installed = { 'lua', 'vim', 'vimdoc', 'go', 'javascript', 'typescript', 'nix', 'typst', 'sql', 'svelte' },
             sync_install = false,
             auto_install = false,
 
