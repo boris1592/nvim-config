ee442f463929cb16e435c84e2d5ca5dffcdc4348
boris1592 <buzin.boria@gmail.com>
2025-06-08 15:58:06 +0300

    feat: move keybind setup to an appropriate place

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
-local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', telescope.find_files)
-vim.keymap.set('n', '<leader>/', telescope.live_grep)
-vim.keymap.set('n', '<leader>b', telescope.buffers)
-
 -- The default binding for this one (and `gD` too btw)
 -- is bullshit. They must've changed alongside adding new
 -- default bindings in v0.11.
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local telescope = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>f', telescope.find_files)
+        vim.keymap.set('n', '<leader>/', telescope.live_grep)
+        vim.keymap.set('n', '<leader>b', telescope.buffers)
+    end,
 }
