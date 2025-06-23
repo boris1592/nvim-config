7d25bcf2caa16681350dc65ceb4dd2eb4b2c3d5f
Boris Buzin <bbuzin@ozon.ru>
2025-06-23 15:55:46 +0300

    feat: lualine

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,8 @@
 -- default bindings in v0.11.
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 
-vim.keymap.set('n', '-', "<CMD>Oil<CR>")
+vim.keymap.set('n', '-', '<CMD>Oil<CR>')
+
+vim.keymap.set('n', 'gp', '<CMD>bprev<CR>')
+vim.keymap.set('n', 'gn', '<CMD>bnext<CR>')
+vim.keymap.set('n', 'gw', '<CMD>bdelete<CR>')
