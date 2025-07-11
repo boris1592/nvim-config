5c07be1a1a0c34c217ab91e067170b7af1db462c
boris1592 <buzin.boria@gmail.com>
2025-07-11 23:16:12 +0300

    feat: add gdscript, refactor lsp setup, update plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,50 +1,83 @@
 return {
     'neovim/nvim-lspconfig',
-    dependencies = {
-        'saghen/blink.cmp',
-        'lukas-reineke/lsp-format.nvim',
-    },
+    dependencies = { 'saghen/blink.cmp', },
     config = function()
-        local lspconfig = require('lspconfig')
-        local lspformat = require('lsp-format')
         local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-        lspformat.setup({})
+        local fmtGroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+        local function onAttachFmt(_, buf)
+            vim.api.nvim_clear_autocmds({ group = fmtGroup, buffer = buf })
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = fmtGroup,
+                buffer = buf,
+                callback = function() vim.lsp.buf.format() end,
+            })
+        end
+
+        -- Just so that the names aren't repeated twice for some LSPs
+        local function setupLsp(name, cfg)
+            vim.lsp.enable(name)
+            vim.lsp.config(name, cfg)
+        end
 
         for _, name in ipairs({
             'lua_ls',
             'rust_analyzer',
             'tinymist',
             'biome',
-            'pyright',
             'svelte',
         }) do
-            lspconfig[name].setup({
-                on_attach = lspformat.on_attach,
+            setupLsp(name, {
                 capabilities = capabilities,
+                on_attach = onAttachFmt,
             })
         end
 
         for _, name in ipairs({
+            'pyright',
             'ts_ls',
         }) do
-            lspconfig[name].setup({})
+            setupLsp(name, { capabilities = capabilities })
         end
 
-        lspconfig.nil_ls.setup({
-            on_attach = lspformat.on_attach,
+        setupLsp('nil_ls', {
             capabilities = capabilities,
+            on_attach = onAttachFmt,
             settings = {
                 ['nil'] = { formatting = { command = { 'alejandra', '--' } } },
             },
         })
 
-        lspconfig.gopls.setup({
-            on_attach = lspformat.on_attach,
+        setupLsp('gopls', {
             capabilities = capabilities,
+            on_attach = onAttachFmt,
             settings = {
                 gopls = { buildFlags = { '-tags=integration' } },
             },
+        })
+
+        setupLsp('gdscript', {
+            capabilities = capabilities,
+            on_attach = function(_, buf)
+                vim.api.nvim_clear_autocmds({ group = fmtGroup, buffer = buf })
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    group = fmtGroup,
+                    buffer = buf,
+                    callback = function()
+                        local lines_cnt = vim.api.nvim_buf_line_count(buf)
+                        local old = vim.api.nvim_buf_get_lines(buf, 0, lines_cnt, false)
+
+                        local proc = vim.system({ 'gdformat', '-' }, { stdin = true })
+                        proc:write(old)
+                        proc:write(nil)
+                        local out = proc:wait().stdout
+
+                        local lines = vim.split(out, '\n', { trimempty = true })
+                        vim.api.nvim_buf_set_lines(buf, 0, lines_cnt, false, lines)
+                    end,
+                })
+            end,
         })
     end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,10 @@ return {
     build = ':TSUpdate',
     config = function()
         require('nvim-treesitter.configs').setup({
-            ensure_installed = { 'lua', 'vim', 'vimdoc', 'go', 'javascript', 'typescript', 'nix', 'typst', 'sql', 'svelte' },
+            ensure_installed = {
+                'lua', 'vim', 'vimdoc', 'rust', 'go', 'javascript', 'typescript', 'nix', 'typst', 'sql', 'svelte',
+                'python', 'gdscript',
+            },
             sync_install = false,
             auto_install = false,
 
