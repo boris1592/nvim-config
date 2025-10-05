385cdfb769c53c86517a05c4a8d66bf300e336a4
boris1592 <buzin.boria@gmail.com>
2025-10-05 15:40:31 +0300

    mini.jump2d instead of leap

--- lua/plugins/leap.lua
+++
@@ -1,6 +0,0 @@
-return {
-  plugins = { 'https://github.com/ggandor/leap.nvim' },
-  config = function()
-    require('leap').set_default_mappings()
-  end,
-}
--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require('mini.files').setup()
 
-    vim.keymap.set('n', '-', function() MiniFiles.open(vim.api.nvim_buf_get_name(0)) end)
+    vim.keymap.set('n', '-', function()
+      MiniFiles.open(vim.api.nvim_buf_get_name(0))
+    end)
   end,
 }
