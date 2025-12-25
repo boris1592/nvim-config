9741cb08943e562f4883dede556d4640f5b5f57a
boris1592 <buzin.boria@gmail.com>
2025-12-26 00:12:51 +0300

    update plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,23 @@
 vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' }, { confirm = false })
 
-require('nvim-treesitter.configs').setup({
-  ensure_installed = {
-    'gdscript',
-    'gleam',
-    'go',
-    'javascript',
-    'lua',
-    'nix',
-    'rust',
-    'sql',
-    'typescript',
-    'typst',
-    'vim',
-    'vimdoc',
-  },
-  sync_install = false,
-  auto_install = false,
+local parsers = {
+  'gdscript',
+  'gleam',
+  'go',
+  'javascript',
+  'lua',
+  'nix',
+  'rust',
+  'sql',
+  'typescript',
+  'typst',
+  'vim',
+  'vimdoc',
+}
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+require('nvim-treesitter').install(parsers)
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = parsers,
+  callback = function() vim.treesitter.start() end,
 })
